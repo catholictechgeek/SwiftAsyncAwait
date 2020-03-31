1be8df18bc91f8c9aa28dd730eb88ab646bb93bb
Steven Nowak <catholictechgeek@gmail.com>
2020-03-30 19:32:38 -0500

    Initial Commit

---
+++ SwiftAsyncAwait/SwiftAsyncAwait.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAsyncAwait.h
+//  SwiftAsyncAwait
+//
+//  Created by Steven Nowak on 3/30/20.
+//  Copyright © 2020 Steven Nowak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftAsyncAwait.
+FOUNDATION_EXPORT double SwiftAsyncAwaitVersionNumber;
+
+//! Project version string for SwiftAsyncAwait.
+FOUNDATION_EXPORT const unsigned char SwiftAsyncAwaitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAsyncAwait/PublicHeader.h>
+
+
